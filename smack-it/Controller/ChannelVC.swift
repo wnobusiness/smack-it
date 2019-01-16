66108093a872dd183a55616c8ecdc4b7e6d731bd
Krishna Torrence <justnerd@Krishnas-MacBook-Pro.local>
2019-01-16 01:12:48 -0500

    end of lesson 2 implemented the slide menu, added gestures to pull it out, and added to view controller (chat and channel)

---
+++ smack-it/Controller/ChannelVC.swift
@@ -0,0 +1,18 @@
+//
+//  ChannelVC.swift
+//  smack-it
+//
+//  Created by Krishna Torrence on 1/16/19.
+//  Copyright © 2019 Krishna Torrence. All rights reserved.
+//
+
+import UIKit
+
+class ChannelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
+
+    }
+}
---
+++ smack-it/Controller/ChatVC.swift
@@ -0,0 +1,23 @@
+//
+//  ChatVC.swift
+//  smack-it
+//
+//  Created by Krishna Torrence on 1/16/19.
+//  Copyright © 2019 Krishna Torrence. All rights reserved.
+//
+
+import UIKit
+
+class ChatVC: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var menuBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        menuBtn.addTarget(self.revealViewController(), action: #selector(revealViewController().revealToggle(_:)), for: .touchUpInside)
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+    }
+    
+}

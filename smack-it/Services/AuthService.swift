88ca97734241b44f0c52415c39f0023e68220c0d
Krishna Torrence <justnerd@Krishnas-MacBook-Pro.local>
2019-01-17 23:58:36 -0500

    End of lesson-06 Added new swift file 'AuthService' - handles login, create user, register user functions, added web request, and set the email and password

---
+++ smack-it/Services/AuthService.swift
@@ -0,0 +1,72 @@
+//
+//  AuthService.swift
+//  smack-it
+//
+//  Created by Krishna Torrence on 1/17/19.
+//  Copyright © 2019 Krishna Torrence. All rights reserved.
+//
+
+import Foundation
+import Alamofire//make web request easy
+
+class AuthService {
+    
+    static let instance = AuthService()
+    
+    //saving data in apps
+    let defaults = UserDefaults.standard
+    
+    //These variables can be accessed anywhere
+    var isLoggedIn: Bool {
+        get {
+            return defaults.bool(forKey: LOGGED_IN_KEY)
+        }
+        set {
+            defaults.set(newValue, forKey: LOGGED_IN_KEY)
+        }
+    }
+    
+    var authToken: String {
+        get {
+            return defaults.value(forKey: TOKEN_KEY) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: TOKEN_KEY)
+        }
+    }
+    
+    var userEmail: String {
+        get {
+            return defaults.value(forKey: USER_EMAIL) as! String
+        }
+        set {
+            defaults.set(newValue, forKey: USER_EMAIL)
+        }
+    }
+    
+    func registerUser(email: String, password: String, completion: @escaping CompletionHandler) {
+        //setting the email and password
+        let lowerCaseEmail = email.lowercased()
+        
+        let header = [
+            "Content-Type" : "application/json; charset=utf-8"
+        ]
+        
+        let body: [String: Any] = [
+            "email": lowerCaseEmail,
+            "password": password
+        ]
+        
+        //Creating web request
+        Alamofire.request(URL_REGISTER, method: .post, parameters: body, encoding: JSONEncoding.default, headers: header).responseString { (response) in
+            
+            if response.result == nil {
+                completion(true)
+            }else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
+    
+}
--- smack-it/Utilities/Constants.swift
+++ smack-it/Utilities/Constants.swift
@@ -8,7 +8,19 @@
 
 import Foundation
 
+//Renaming a type
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+//URL Constants
+let BASE_URL = "https://smack-it.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
+
 //Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+//User Defaults
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"

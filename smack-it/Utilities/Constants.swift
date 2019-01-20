bc92fe4b7cb8d7c548837e9e7f2439fc9784be98
Krishna Torrence <justnerd@Krishnas-MacBook-Pro.local>
2019-01-20 18:43:32 -0500

    End of lesson-10, add AvatarPicker VC, set up interface, and created custom class for avatar cell

--- smack-it/Utilities/Constants.swift
+++ smack-it/Utilities/Constants.swift
@@ -16,6 +16,7 @@ let BASE_URL = "https://smack-it.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
 let URL_LOGIN = "\(BASE_URL)account/login"
 let URL_USER_ADD = "\(BASE_URL)user/add"
+let TO_AVATAR_PICKER = "toAvatarPicker"
 
 //Segues
 let TO_LOGIN = "toLogin"
---
+++ smack-it/View/AvatarCell.swift
@@ -0,0 +1,25 @@
+//
+//  AvatarCell.swift
+//  smack-it
+//
+//  Created by Krishna Torrence on 1/20/19.
+//  Copyright © 2019 Krishna Torrence. All rights reserved.
+//
+
+import UIKit
+
+class AvatarCell: UICollectionViewCell {
+    
+    @IBOutlet weak var avatarImg: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        setUpView()
+    }
+    
+    func setUpView() {
+        self.layer.backgroundColor = UIColor.lightGray.cgColor
+        self.layer.cornerRadius = 10
+        self.clipsToBounds = true
+    }
+}

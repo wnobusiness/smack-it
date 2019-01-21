8a3605748fd7fb252bd78849723cbe31249d6509
Krishna Torrence <justnerd@Krishnas-MacBook-Pro.local>
2019-01-20 21:02:44 -0500

    End lesson-12, set up avatar background color, added CircleImage.swift to have the image circle, change the font color in create account, add activity indicator, added tap gesture, created Notification Post

--- smack-it/Utilities/Constants.swift
+++ smack-it/Utilities/Constants.swift
@@ -18,6 +18,12 @@ let URL_LOGIN = "\(BASE_URL)account/login"
 let URL_USER_ADD = "\(BASE_URL)user/add"
 let TO_AVATAR_PICKER = "toAvatarPicker"
 
+//Colors
+let smackPurplePlaceholder = #colorLiteral(red: 0.3254901961, green: 0.4196078431, blue: 0.7764705882, alpha: 0.86)
+
+//Notifications Constants
+let NOTIF_USER_DATA_DID_CHANGE = Notification.Name("notifUserDataChanged")
+
 //Segues
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
---
+++ smack-it/View/CircleImage.swift
@@ -0,0 +1,28 @@
+//
+//  CircleImage.swift
+//  smack-it
+//
+//  Created by Krishna Torrence on 1/20/19.
+//  Copyright © 2019 Krishna Torrence. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CircleImage: UIImageView {
+
+    override func awakeFromNib() {
+        setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupView()
+    }
+
+}

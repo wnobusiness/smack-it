fe8c8487e7e8e4542e1c2dd98253496acfd2f682
Krishna Torrence <justnerd@Krishnas-MacBook-Pro.local>
2019-01-20 19:31:01 -0500

    set up segement control for light/dark avatar, user can now select avatar, adjust collection view cell space, End of lesson-11

--- smack-it/View/AvatarCell.swift
+++ smack-it/View/AvatarCell.swift
@@ -8,6 +8,11 @@
 
 import UIKit
 
+enum AvatarType {
+    case dark
+    case light
+}
+
 class AvatarCell: UICollectionViewCell {
     
     @IBOutlet weak var avatarImg: UIImageView!
@@ -15,6 +20,17 @@ class AvatarCell: UICollectionViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         setUpView()
+    }
+    
+    //Setting up the segment control to light or dark avatar
+    func configureCell(index: Int, type: AvatarType){
+        if type == AvatarType.dark {
+            avatarImg.image = UIImage(named: "dark\(index)")
+            self.layer.backgroundColor = UIColor.lightGray.cgColor
+        }else {
+            avatarImg.image = UIImage(named: "light\(index)")
+            self.layer.backgroundColor = UIColor.gray.cgColor
+        }
     }
     
     func setUpView() {

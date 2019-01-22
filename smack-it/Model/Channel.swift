91643b921c76e9d76d394821e059d331bfc6b0f0
Krishna Torrence <justnerd@Krishnas-MacBook-Pro.local>
2019-01-22 16:42:37 -0500

    set channel added MessageService.swift, and added model 'Channel.swift'.

---
+++ smack-it/Model/Channel.swift
@@ -0,0 +1,17 @@
+//
+//  Channel.swift
+//  smack-it
+//
+//  Created by Krishna Torrence on 1/22/19.
+//  Copyright © 2019 Krishna Torrence. All rights reserved.
+//
+
+import Foundation
+
+struct Channel {
+    public private(set) var channelTitle: String!
+    public private(set) var channelDescription: String!
+    public private(set) var id: String!
+    
+    
+}
---
+++ smack-it/Services/MessageService.swift
@@ -0,0 +1,44 @@
+//
+//  MessageService.swift
+//  smack-it
+//
+//  Created by Krishna Torrence on 1/22/19.
+//  Copyright © 2019 Krishna Torrence. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+import SwiftyJSON
+
+class MessageService {
+    
+    static let instance = MessageService()
+    
+    var channels = [Channel]()
+    
+    func findAllChannel(completion: @escaping CompletionHandler) {
+        Alamofire.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEARDER).responseJSON { (response) in
+            
+            if response.result.error == nil {
+                guard let data = response.data else { return }
+                if let json = JSON(data: data).array {
+                    for item in json {
+                        let name = item["name"].stringValue
+                        let channelDescription = item["description"].stringValue
+                        let id = item["id"].stringValue
+                        let channel = Channel(channelTitle: name, channelDescription: channelDescription, id: id)
+                        self.channels.append(channel)
+                    }
+                    print(self.channels[0].channelTitle)
+                    completion(true)
+                }
+                
+                
+            }else {
+                completion(false)
+                debugPrint(response.result.error as Any)
+            }
+        }
+    }
+    
+}

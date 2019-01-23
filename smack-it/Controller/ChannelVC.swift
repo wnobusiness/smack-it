1aa6887d921843f1db54bf8df4b839e125dc5762
Krishna Torrence <justnerd@Krishnas-MacBook-Pro.local>
2019-01-23 01:10:22 -0500

    Add 'SocketService.swift'setup the web sockets for the channels

--- smack-it/Controller/ChannelVC.swift
+++ smack-it/Controller/ChannelVC.swift
@@ -26,6 +26,12 @@ class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
         //Notification observer
         NotificationCenter.default.addObserver(self, selector: #selector(ChannelVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+        
+        SocketService.instance.getChannel { (success) in
+            if success {
+                self.tableView.reloadData()
+            }
+        }
 
     }
     
---
+++ smack-it/Services/SocketService.swift
@@ -0,0 +1,50 @@
+//
+//  SocketService.swift
+//  smack-it
+//
+//  Created by Krishna Torrence on 1/22/19.
+//  Copyright © 2019 Krishna Torrence. All rights reserved.
+//
+
+import UIKit
+import SocketIO
+
+class SocketService: NSObject {
+
+    static let instance = SocketService()
+    
+    let manager: SocketManager
+    let socket: SocketIOClient
+    
+    override init() {
+        self.manager = SocketManager(socketURL: URL(string: BASE_URL)!)
+        self.socket = manager.defaultSocket
+        super.init()
+    }
+    //Opening up the connection btw the API and Client
+    func establishConnection() {
+        socket.connect()
+    }
+    
+    func closeConnection() {
+        socket.disconnect()
+    }
+    
+    func addChannel(channelName: String, channelDescription: String, completion: @escaping CompletionHandler) {
+        socket.emit("newChannel", channelName, channelDescription)
+        completion(true)
+    }
+    
+    func getChannel(completion: @escaping CompletionHandler) {
+        socket.on("channelCreated") { (dataArray, ack) in
+            guard let channelName = dataArray[0] as? String else { return }
+            guard let channelDesc = dataArray[1] as? String else  { return }
+            guard let channelId = dataArray[2] as? String else { return }
+            
+            let newChannel = Channel(channelTitle: channelName, channelDescription: channelDesc, id: channelId)
+            
+            MessageService.instance.channels.append(newChannel)
+            completion(true)
+        }
+    }
+}

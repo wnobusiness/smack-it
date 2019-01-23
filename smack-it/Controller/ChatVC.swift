b897f77aded992d9ffc536e9d72b6d18bbd40c50
Krishna Torrence <justnerd@Krishnas-MacBook-Pro.local>
2019-01-23 14:22:32 -0500

    End of lesson-22, set up sending messages

--- smack-it/Controller/ChatVC.swift
+++ smack-it/Controller/ChatVC.swift
@@ -13,9 +13,12 @@ class ChatVC: UIViewController {
     //Outlets
     @IBOutlet weak var menuBtn: UIButton!
     @IBOutlet weak var channelNameLbl: UILabel!
-    
+    @IBOutlet weak var messageTxtBox: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.bindToKeyboard()
+        let tap = UITapGestureRecognizer(target: self, action: #selector(ChatVC.handleTap))
+        view.addGestureRecognizer(tap)
         //The button for the menu (toggle the menu)
         menuBtn.addTarget(self.revealViewController(), action: #selector(revealViewController().revealToggle(_:)), for: .touchUpInside)
         //sliding gestures
@@ -47,11 +50,30 @@ class ChatVC: UIViewController {
         updateWithChannel()
     }
     
+    @objc func handleTap() {
+        view.endEditing(true)
+    }
+    
     func updateWithChannel() {
         let channelName = MessageService.instance.selectedChannel?.channelTitle ?? ""
         channelNameLbl.text = "#\(channelName)"
         getMessages()
     }
+    
+    @IBAction func sendMsgPressed(_ sender: Any) {
+        if AuthService.instance.isLoggedIn {
+            guard let channelId = MessageService.instance.selectedChannel?.id else { return }
+            guard let message = messageTxtBox.text else { return }
+            
+            SocketService.instance.addMessage(messageBody: message, userId: UserDataService.instance.id, channelId: channelId) { (success) in
+                if success {
+                    self.messageTxtBox.text = ""
+                    self.messageTxtBox.resignFirstResponder()
+                }
+            }
+        }
+    }
+    
     
     func onLoginGetMessages() {
         MessageService.instance.findAllChannel { (success) in

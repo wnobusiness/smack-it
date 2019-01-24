631aa54c9a5be434ded2aa14524bd8128f56c7fa
Krishna Torrence <justnerd@Krishnas-MacBook-Pro.local>
2019-01-23 20:26:10 -0500

    End lesson-23 set up chat screen and added 'MessageCell.swift'.

--- smack-it/Services/MessageService.swift
+++ smack-it/Services/MessageService.swift
@@ -43,15 +43,15 @@ class MessageService {
     }
     
     func findAllMessagesForChannel(channelId: String, completion: @escaping CompletionHandler) {
-        Alamofire.request("\(URL_GET_MESSAGES)\(channelId)", method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEARDER).responseJSON { (response) in
+        Alamofire.request(URL_GET_MESSAGES + channelId, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEARDER).responseJSON { (response) in
             if response.result.error == nil {
                 self.clearMessages()
                 guard let data = response.data else { return }
                 if let json = JSON(data: data).array {
                     for item in json{
                         let messageBody = item["messageBody"].stringValue
-                        let channelId = item["channelid"].stringValue
-                        let id = item["_id"].stringValue
+                        let channelId = item["channelId"].stringValue
+                        let id = item["userId"].stringValue
                         let userName = item["userName"].stringValue
                         let userAvatar = item["userAvatar"].stringValue
                         let userAvatarColor = item["userAvatarColor"].stringValue
--- smack-it/Utilities/Constants.swift
+++ smack-it/Utilities/Constants.swift
@@ -19,7 +19,7 @@ let URL_USER_ADD = "\(BASE_URL)user/add"
 let TO_AVATAR_PICKER = "toAvatarPicker"
 let URL_USER_BY_EMAIL = "\(BASE_URL)user/byEmail/"
 let URL_GET_CHANNELS = "\(BASE_URL)channel/"
-let URL_GET_MESSAGES = "\(BASE_URL)message/byChannel"
+let URL_GET_MESSAGES = "\(BASE_URL)message/byChannel/"
 
 //Colors
 let smackPurplePlaceholder = #colorLiteral(red: 0.3254901961, green: 0.4196078431, blue: 0.7764705882, alpha: 0.86)
